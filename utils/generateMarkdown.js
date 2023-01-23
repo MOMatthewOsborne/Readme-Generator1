4479aea378028af0a909eb8d33355debb964b35c
Matthew Osborne <momatthewosborne1@gmail.com>
2023-01-23 15:43:09 +0000

    successfully generating README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,15 @@
+function renderBadge(license) {
+  if (license === "MIT") {
+    return "[![MIT](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)"
+  }
+  else{
+    return ""
+  }
+}
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-# 
+${renderBadge(data.license)}
 
 # Description
  ${data.description}
